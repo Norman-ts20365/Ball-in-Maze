260ebc0e374c6936561e5ff0d2d25168934b8d30
Shikieiki555 <78961712+Shikieiki555@users.noreply.github.com>
2023-02-28 14:19:53 +0000

    Update ball_tracking.py

--- ball_tracking.py
+++ ball_tracking.py
@@ -3,13 +3,14 @@ import numpy as np
 import cv2
 import imutils
 import matplotlib.pyplot as plt
+import time,os
 
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space, then initialize the
 # list of tracked points 
 greenLower = (50, 86, 6)
 greenUpper = (90, 255, 255)
-
+count=0
 vid = cv2.VideoCapture(0)
 #looping
 while True:
@@ -23,22 +24,33 @@ while True:
 
 	# construct a mask for the color green
 	mask = cv2.inRange(hsv, greenLower, greenUpper)
-                                                                                                                                                                                                                                                                                                                                                                                                                    
+
 	# find contours in the mask and initialize the current
 	# (x, y) center of the ball
 	cnts, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 	if len(cnts) > 0:
 		c = max(cnts, key=cv2.contourArea)
-		if cv2.contourArea(c) > 50:
+		if cv2.contourArea(c) > 50 and count<100:
+			count=0
 			x,y,w,h =cv2.boundingRect(c)
 			cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),2)
 			center=(int(x+w/2),int(y+h/2))
 			print(center)
 			cv2.circle(frame, center, 5, (0, 0, 255), -1)
+		elif cv2.contourArea(c) < 50 and count<100:
+			center="ball disappeared"
+			count=count+1
+			print(center)
+			print(count)
+		else:
+			print("ball droped")
+			break
+		
 
 	cv2.imshow("Frame", frame)
 	cv2.imshow("mask",mask)
 	cv2.imshow("hsv",hsv)
+
 	key = cv2.waitKey(1) & 0xFF
 
 	# if the 'q' key is pressed, stop the loop
