f91ba63574ea833489b8eb2a7ecee3694ea46506
Shikieiki555 <78961712+Shikieiki555@users.noreply.github.com>
2023-03-09 14:12:47 +0000

    Update ball_tracking.py

--- ball_tracking.py
+++ ball_tracking.py
@@ -4,8 +4,17 @@ import cv2
 import imutils
 import matplotlib.pyplot as plt
 import time,os
+import serial
+import sys
 from datetime import datetime
 from time import strftime
+from time import sleep
+
+ser=serial.Serial()
+ser.baudrate =115200
+ser.port='COM5'
+ser.open()
+test_packets=[]
 
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space, then initialize the
@@ -41,7 +50,7 @@ while True:
 		arrow=cv2.arrowedLine(frame, a, b,(0,255,0), 2, 8, 0,0.1)
 	if len(cnts) > 0:
 		c = max(cnts, key=cv2.contourArea)
-		if cv2.contourArea(c) > 75 and count<100:
+		if cv2.contourArea(c) > 25 and count<100:
 			count=0
 			x,y,w,h =cv2.boundingRect(c)
 			cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),2)
@@ -53,9 +62,8 @@ while True:
 
 			X1=int(x+w/2)
 			Y1=int(y+h/2)
-			packet=(X1,Y1,)
 		
-		elif cv2.contourArea(c) < 75 and count<100:
+		elif cv2.contourArea(c) < 25 and count<100:
 			center="ball disappeared"
 			count=count+1
 		
@@ -66,17 +74,21 @@ while True:
 		if len(centers) % 20 == 0 and len(centers) >19 and arrowcount==len(centers):
 			a=centers[arrowcount-20]
 			b=centers[arrowcount-1]
+			print(a[0],a[1])
 			arrowcount=0
 			centers=[]
 			check=1
 			min=datetime.now().strftime("%M")
 			sec=datetime.now().strftime("%S")
 			microsec=datetime.now().strftime("%f")
+			microsec1=int(str(microsec)[:2])
+			microsec2=int(str(microsec)[2:4])
+			microsec3=int(str(microsec)[4:])
 			print(min)
 			print(sec)
 			print(microsec)
-
-			packet=("")
+			#test_packet=bytearray([50,52,a[0],a[1],min,sec,microsec1,microsec2,microsec3,51])
+		
 
 	cv2.imshow("Frame", frame)
 	cv2.imshow("mask",mask)
