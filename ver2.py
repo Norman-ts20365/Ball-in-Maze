b7ece9cf72079366ed27f1e578e7dfa9c4f6bc76
Shikieiki555 <78961712+Shikieiki555@users.noreply.github.com>
2023-03-17 02:51:53 +0000

    Create ver2.py
    Second version with serial and ball_track divided into functions, tried to use multiprocess but failed
    Can add board detection afterward

---
+++ ver2.py
@@ -0,0 +1,109 @@
+from collections import deque
+import numpy as np
+import cv2
+import imutils
+import matplotlib.pyplot as plt
+import time,os
+# import serial
+# import sys
+from datetime import datetime
+from time import strftime
+import multiprocessing as mp
+
+# from time import sleep
+
+# ser=serial.Serial()
+# ser.baudrate =115200
+# ser.port='COM4'
+# ser.open()
+# test_packets=[]
+
+# define the lower and upper boundaries of the "green"
+# ball in the HSV color space, then initialize the
+# list of tracked points 
+test_packet=[]
+greenLower = (50, 86, 6)
+greenUpper = (90, 255, 255)
+
+def serial_IO(test_packet):
+	serialout=bytearray(test_packet)
+	print(serialout)
+	# ser.write(test_packet)
+			# while (ser.in_waiting > 0):
+			# 	print(ser.read_until().decode("utf-8"), end = '') 
+			# print("")
+
+#looping
+def ball_track():
+
+	# greenLower = (50, 86, 6)
+	# greenUpper = (90, 255, 255)
+	count=0
+	centers=[]
+	arrow=0
+	arrowcount=0
+	a=0
+	b=0
+	check=0
+
+	if len(cnts) > 0:
+		c = max(cnts, key=cv2.contourArea)
+		if cv2.contourArea(c) > 25 and count<100:
+			count=0
+			x,y,w,h =cv2.boundingRect(c)
+			cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),2)
+			center=(int(x+w/2),int(y+h/2))
+			xcor=int(x+w/2)
+			ycor=int(y+h/2)
+			print(center)
+			cv2.circle(frame, center, 5, (0, 0, 255), -1)
+			centers.append(center)
+			arrowcount=arrowcount+1
+			min=datetime.now().strftime("%M")
+			min=int(min)
+			sec=datetime.now().strftime("%S")
+			sec=int(sec)
+			microsec=datetime.now().strftime("%f")
+			microsec1=int(str(microsec)[:2])
+			microsec2=int(str(microsec)[2:4])
+			microsec3=int(str(microsec)[4:])
+			global test_packet,starter
+			test_packet=([2,4,xcor,ycor,min,sec,microsec1,microsec2,microsec3,3])
+			return test_packet
+
+		
+		elif cv2.contourArea(c) < 25 and count<100:
+			center="ball disappeared"
+			count=count+1
+		
+		elif count==100:
+			print("ball droped")
+
+	
+if __name__ == '__main__':
+	vid = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+	while True:
+		ret,frame=vid.read()
+		frame = imutils.resize(frame, width=255)
+		blurred = cv2.GaussianBlur(frame, (5, 5), 0)
+		hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)	
+		# construct a mask for the color green
+		mask = cv2.inRange(hsv, greenLower, greenUpper)
+		# find contours in the mask and initialize the current
+		cnts, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+		ball_track()
+		# p1=mp.Process(target=serial_IO,args=(test_packet))
+		# p1.start()
+		# p1.join()
+		cv2.imshow("Frame", frame)
+		cv2.imshow("mask",mask)
+		cv2.imshow("hsv",hsv)
+
+		serial_IO(test_packet)
+
+		key = cv2.waitKey(1) & 0xFF
+		 		# if the 'q' key is pressed, stop the loop
+		if key == ord("q"):
+			break
+
