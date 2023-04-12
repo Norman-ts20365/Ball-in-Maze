9d604b4394ce95557a35c022a01a84d0b207c894
Norman Cheen <normancheen.en@gmail.com>
2023-04-12 16:08:03 +0100

    Update Maze_Adjustment.py

--- Maze_Adjustment.py
+++ Maze_Adjustment.py
@@ -1,11 +1,23 @@
 import serial
-from Maze_Driver import serial_read
 
 # Packet Pre-declaration
 jog_Yplus = bytearray([2,7,119,3]) # 2,7,W,3
 jog_Yminus = bytearray([2,7,115,3])
 jog_Xplus = bytearray([2,7,100,3]) # STX, JOG_TYPE, D, ETX
 jog_Xminus = bytearray([2,7,97,3])
+
+
+def serial_read(command):
+    # Serial object
+    ser = serial.Serial()
+    ser.baudrate = 115200
+    ser.port = 'COM4'
+    ser.open()
+    while True:
+        ser.write(command)
+        if (ser.in_waiting > 0):
+            print(ser.read_until().decode("utf-8"), end = '')
+
 
 def user_command(command):
     if command == 'w':
