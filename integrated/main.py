f40d97f448aa266c09dccb425bcd94e7d4092016
Norman Cheen <normancheen.en@gmail.com>
2023-04-18 13:30:38 +0100

    Update and rename main to main.py

--- integrated/main.py
+++ integrated/main.py
@@ -14,10 +14,10 @@ def serial_read(queue):
     ser.port = 'COM4'
     ser.open()
     while True:
-        # if not queue.empty():
-        #     cmd = queue.get() # sharing from another process the serial write command
-        #     ser.write(cmd)
-        #     #print("Sent: ", cmd)
+        if not queue.empty():
+            cmd = queue.get() # sharing from another process the serial write command
+            ser.write(cmd)
+            #print("Sent: ", cmd)
         if (ser.in_waiting > 0):
             print(ser.read_until().decode("utf-8"), end = '')
         return
@@ -31,19 +31,19 @@ if __name__ == "__main__":
     condition = Condition()
 
     # declare processes
-    process_gui = Process(target=gui_main, args=(condition,))
-    # process_test = Process(target=testfunc, args=(condition,))
+    process_gui = Process(target=gui_main, args=(condition,queue,))
+    process_test = Process(target=testfunc, args=(condition,))
     # process2_Maze_Driver = Process(target=  , args=(condition,))
     # process_serial_read = Process(target=serial_read, args=(queue,))
 
     # run processes in parallel
     process_gui.start()
-    # process_test.start()
+    process_test.start()
     # process2_Maze_Driver.start()
     # process_serial_read.start()
 
     # wait for processes to return 
     process_gui.join()
-    # process_test.join()
+    process_test.join()
     # process2_Maze_Driver.join()
     # process_serial_read.join()
