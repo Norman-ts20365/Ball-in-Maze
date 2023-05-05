ba9a8ca6d2f216dddaffa0d4e3f796cbbf05af6d
Norman Cheen <normancheen.en@gmail.com>
2023-05-05 10:42:17 +0100

    Update Route_Detection.py

--- integrated/Route_Detection.py
+++ integrated/Route_Detection.py
@@ -1,3 +1,10 @@
+"""
+Code written by: Norman Cheen
+Automated Route Detection System
+ts20365@bristol.ac.uk
+University of Bristol
+"""
+
 import sys
 import numpy as np
 import cv2 
@@ -6,12 +13,6 @@ from skimage.morphology import skeletonize_3d
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import pickle
-# from multiprocessing import Queue
-# import os
-# import imutils
-# import serial
-# from time import sleep
-# import csv
 
 from Board_Detection import board_detection
 from Serial import serial_read
@@ -26,45 +27,34 @@ def find_target_contour_area(bin_image, template):
     """
 
     # additional blurring and thresholding to smooth out edges and remove any effect of reflection
-    bin_image = cv2.GaussianBlur(bin_image, (5, 5), cv2.BORDER_DEFAULT) 
+    bin_image = cv2.GaussianBlur(bin_image, (5, 5), cv2.BORDER_DEFAULT)
     ret,bin_image = cv2.threshold(bin_image, 150 , 255, cv2.THRESH_BINARY)
 
     # template matching to locate the position of the arrow on the maze
     res = cv2.matchTemplate(bin_image, template, cv2.TM_SQDIFF)
-    # plt.imshow(res, cmap='gray')
+    # For TM_SQDIFF, Good match yields minimum value, thus min_loc is used
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res) 
-    # ^ For TM_SQDIFF, Good match yields minimum value; bad match yields large values
 
     h, w = template.shape[::]   # height and width of the arrow
     top_left = min_loc          # coordinates of the top left corner of the arrow
     bottom_right = [top_left[0] + w, top_left[1] + h] 
     centre = [int(top_left[0] + w/2), int(top_left[1] + h/2)] # the centre of the arrow
-    # print(centre)
     cv2.rectangle(img_gray_default, top_left, bottom_right, 10, 2)  # draw a black rectangle around the detected arrow
 
     target = np.array([centre[0]-2*w, centre[1]])   # a pixel that might be within the route
-    # print(target)
     target_range = np.array([target + x for x in range(-3,3)]) # a range of pixels that might be within the route (for buffer)
     target_range = target_range.tolist()
-    # print(target_range)
-    # x,y = target_range.T
-    # plt.plot(x,y,"r*")
-    
-    # plt.imshow(img_gray_default)
-    # plt.show()
-    # cv2.imshow("Matched image", img_gray_default)
-    # cv2.waitKey()
-    # cv2.destroyAllWindows()  
 
     return target_range
 
 def find_route_contour(bin_image, target_range):
     """
     This function finds the contours of the route and verify the result automatically.
-    It starts by finding the nth largest contour, then (n-1)th contour if result is incorrect to
-    finally obtain the correct contour of the route.
-    :param bin_image : a binary (black and white) image
-    :return          : pixel coordinates (type: list)
+    It starts by finding the nth largest contour, then (n+1)th largest contour if result 
+    is incorrect to finally obtain the correct contour of the route.
+    :param bin_image    : a binary (black and white) image
+    :param target_range : a range of pixel coordinates that might intersect the route contour
+    :return             : detected contour
     """
     bin_image = bin_image.astype(np.uint8)  # Cast ndarray to unsigned 8-bit integer
     contours, hierarchy = cv2.findContours(bin_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -77,7 +67,7 @@ def find_route_contour(bin_image, target_range):
             # Compute and display the largest contour area
             cur_contour = contours_sorted[contour_num]
 
-            # image method:
+            # Visualising the detected contour
             image_contour = np.copy(img_coloured)
             image_contour = cv2.drawContours(image_contour, [cur_contour], 0, (0, 255, 0), 2, cv2.LINE_AA, maxLevel=1)
 
@@ -86,6 +76,7 @@ def find_route_contour(bin_image, target_range):
             cur_contour_outline = np.column_stack((X,Y))
             cur_contour_outline = cur_contour_outline.tolist()
 
+            # # Optional: Plotting to double check the intersecting line
             # x2,y2 = target_range.T
             # plt.plot(x2,-y2,"r-")
             # plt.plot(X,-Y,"g*")
@@ -95,14 +86,6 @@ def find_route_contour(bin_image, target_range):
             # cv2.destroyAllWindows()
             
             if any(target in target_range for target in cur_contour_outline): # if target range intersect with current contour
-            # intersection = [x for x in target_range if x in cur_contour_outline]
-            # print(intersection)
-            # if len(intersection) > 0: 
-                # cv2.imshow('Contour', image_contour)
-                # # plt.imshow(image_contour)
-                # # plt.show()
-                # cv2.waitKey(0)  # display window until any keypress
-                # cv2.destroyAllWindows()
                 break
             else:
                 contour_num -= 1
@@ -124,14 +107,8 @@ def extract_route(bin_image, contour):
     """
     # create a black `mask` the same size as the original grayscale image 
     mask = np.zeros_like(bin_image)
-    # fill the new mask with the shape of the largest contour
-    # all the pixels inside that area will be white
+    # fill the new mask with the shape of the largest contour in white
     cv2.fillPoly(mask, [contour], 255)
-
-    # # Skeletonise the image
-    # route_skeleton = skeletonize_3d(mask)
-    # # skel = cv2.cv2tColor(skeleton.astype(np.uint8),cv2.COLOR_GRAY2BGR)
-    # cv2.imshow('skeleton',route_skeleton)
 
     # create a copy of the current mask
     res_mask = np.copy(mask)
@@ -140,8 +117,8 @@ def extract_route(bin_image, contour):
     res_mask[mask == 255] = cv2.GC_FGD # obvious foreground pixels
 
     # create a mask for obvious and probable foreground pixels
-    # all the obvious foreground pixels will be white and...
-    # ... all the probable foreground pixels will be black
+    # obvious foreground pixels will be white
+    # probable foreground pixels will be black
     mask2 = np.where(
     (res_mask == cv2.GC_FGD) | (res_mask == cv2.GC_PR_FGD),
     255,
@@ -153,49 +130,43 @@ def extract_route(bin_image, contour):
     mask3d = new_mask3d
     mask3d[new_mask3d > 0] = 255.0
     mask3d[mask3d > 255] = 255.0
-    # apply Gaussian blurring to smoothen out the edges a bit
-    # `mask3d` is the final foreground mask (not extracted foreground image)
-    mask3d = cv2.GaussianBlur(mask3d, (5, 5), 0)
-    # # mask3d = cv2.medianBlur(mask3d,5)
-    # cv2.imshow('Foreground mask', mask3d)
-    # cv2.waitKey(0)  # display window until any keypress
-    # cv2.destroyAllWindows()
 
-    # Skeletonise the image
+    # apply Gaussian blurring to smoothen out the edges again
+    mask3d = cv2.GaussianBlur(mask3d, (5, 5), 0)
+
+    # skeletonise the image
     skeleton = skeletonize_3d(mask3d)
-    # cv2.imshow('skeleton', skeleton)
 
     return skeleton
 
 def find_route_coordinates(skeleton_image):
     """
     This function finds the pixel coordinates of the route
-    :param skeletonised_image   : a skeletonised image with the route in green 
+    :param skeletonised_image   : a skeletonised image of the route 
     :return                     : pixel coordinates of the route
     """
-    # Obtain the coordinates of the pixels forming the route
+    # Obtain the coordinates of the pixels forming the route (in green)
     green = [0,255,0]
     Y, X = np.where(np.all(skeleton_image==green,axis=2))
     coordinates = np.column_stack((X,Y))
-    # print(coordinates)
-    # print("Total coordinates:" , len(coordinates))
 
     return coordinates
 
 def find_end_points(skeleton_image):
     """
-    This function finds the start and end points of the route
+    This function finds the start and end points of the route using
+    Hit-or-miss Transform
     :param image: skeleton image of the route
     :return     : coordinates of start and end points of the route
     """
-    # Finding the starting and end points of the route
-    # kernels to find all possible endpoint patterns 
+    # Finding the start and end points of the route
+    # kernels to find all possible endpoint patterns:
     k1 = np.array(([0, 0, -1], [-1, 1, -1], [-1, -1, -1]), dtype="int")
     k2 = np.array(([-1, -1, -1], [0, 1, -1], [0, -1, -1]), dtype="int")
     k3 = np.array(([-1, -1, 0],  [-1, 1, 0], [-1, -1, -1]), dtype="int")
     k4 = np.array(([-1, -1, -1], [-1, 1, -1], [-1, 0, 0]), dtype="int")
 
-    # convert BGR image to gray scale (black and white)
+    # convert BGR image to gray scale
     route_skeleton_gray = cv2.cvtColor((skeleton_image).astype('uint8'),cv2.COLOR_BGR2GRAY)
 
     # perform hit-miss transform for every kernel (output type = array)
@@ -204,27 +175,28 @@ def find_end_points(skeleton_image):
     o3 = cv2.morphologyEx(route_skeleton_gray, cv2.MORPH_HITMISS, k3)
     o4 = cv2.morphologyEx(route_skeleton_gray, cv2.MORPH_HITMISS, k4)
 
-    # add the results of all the above 4
+    # add the results of all the 4 outputs
     hitmiss_out = o1 + o2 + o3 + o4
 
     # find none-zero points (ie. end points) First element = start, Second element = end
     end_points = np.argwhere(hitmiss_out)
 
-    # ploting the determined end-points for checking
-    img_end_points = route_skeleton.copy()
-    for pt in end_points:
-        pt[1],pt[0] = pt[0],pt[1] # invert so that format == [x,y]
-        img_end_points = cv2.circle(img_end_points, (pt[0], pt[1]), 15, (0,0,255), -1)
-        # print(pt)
+    # # Optional: Ploting the determined end-points for checking
+    # img_end_points = route_skeleton.copy()
+    # for pt in end_points:
+    #     pt[1],pt[0] = pt[0],pt[1] # invert so that format == [x,y]
+    #     img_end_points = cv2.circle(img_end_points, (pt[0], pt[1]), 15, (0,0,255), -1)
+    #     # print(pt)
     # cv2.imshow('endpoint',img_end_points)
-    print("\n The start and end points are:\n" , end_points)
+    # print("\n The start and end points are:\n" , end_points)
 
     return end_points
 
 def find_neighbour(cur_coordinates):
     """
     This function finds the 8 neighbours surrounding a point and return their coordinates in an array
-    :param cur_coordinates: the coordinates of the point that's surrounded by the neighbours
+    :param cur_coordinates  : coordinates of a point 
+    :return                 : an array consisting of the coodinates of its neighbours
     """
     neighbours = []
     for n in range(-1,2):
@@ -243,89 +215,46 @@ def sort_route(start_pt,end_pt,all_pts):
     :param start_pt: the coordinates of the starting point [array]
     :param end_pt  : the coordinates of the starting point
     :param all_pts : the coordinates of all the points in the route
+    :return        : a list of sorted coordinates of the route
     """
     cur_pt = start_pt.tolist()
     coordinates = (all_pts.tolist())
-    # print("\n \n old, unsorted: \n \n" , coordinates)
+
     sorted_coordinates = [cur_pt]
     while np.array_equal(cur_pt, end_pt) == False:
         cur_neighbour = find_neighbour(cur_pt).tolist()             # get neighbours and convert to a list
         next_pt = [x for x in cur_neighbour if x in coordinates][0] # common element of 2 lists
         # print(next_pt)
         sorted_coordinates.append(next_pt)      
-        coordinates.remove(next_pt)         # delete determined point
+        coordinates.remove(next_pt)         # remove determined point
         cur_pt = next_pt
+
     return sorted_coordinates
-
-# def animation_plot(x_cor, y_cor):
-#         # Create a figure and axis
-#         fig, ax = plt.subplots()
-#         fig.patch.set_facecolor('black')
-
-#         # Set the axis limits
-#         ax.set_xlim(0, 765)
-#         ax.set_ylim(0, 635)
-#         ax.set_ylim(ax.get_ylim()[::-1])      # invert the axis
-#         ax.set_axis_off()
-#         # ax.set_facecolor('black')
-
-#         # Create a line object
-#         line, = ax.plot([], [], lw=2)
-
-#         # Define the initialization function
-#         def plt_init():
-#             line.set_data([], [])
-#             return line,
-
-#         # Define the update function
-#         def update(frame):
-#             # Get the x and y values up to the current frame number
-#             x_data = x_cor[:frame+1]
-#             y_data = y_cor[:frame+1]
-            
-#             # Set the data for the line object
-#             line.set_data(x_data, y_data)
-#             return line,
-
-#         # Create the animation object
-#         ani = animation.FuncAnimation(fig, update, interval=0, frames=len(x_cor), init_func=plt_init, blit=True, repeat=False)
-
-#         # Display the animation
-#         # plt.show()
 
 
 def route_detection_main():
+    """
+    This is the main function that initiates the route detection process.
+    """
     global img_gray_default, img_gray, img_coloured, img_bin, target_route, route_contour, route_skeleton, route_coordinates, end_points, sorted_route_coordinates
-    # Read Image-----------------------------------------------------------------------------------------------------------------------------------------------
-    # path = r'C:\Users\Asus\Desktop\CodeGP3\path_detection\maze_image_medium.png'
-    # img_gray_default = cv2.imread(path,0)  # "0" means read image in gray scale
 
     # Read Image-----------------------------------------------------------------------------------------------------------------------------------------------
-    detected_board = board_detection()
+    detected_board = board_detection()    # raw, cropped image of the maze
     img_gray_default = cv2.cvtColor(detected_board,cv2.COLOR_BGR2GRAY)
 
 
-    # cv2.imshow('Ori gray', img_gray)
-    tem_path = r'C:\Desktop\Desktop Summary\EEE\groupproject\arrow.png'
+    tem_path = r'C:\Desktop\Desktop Summary\EEE\groupproject\arrow.png'  # path of the template image
     tem = cv2.imread(tem_path, 0)
 
     # # Resize image
-    # scale_percent = 80 # percent of original size
-    # width = int(img_gray.shape[1] * scale_percent / 100)
-    # height = int(img_gray.shape[0] * scale_percent / 100)
-    # dim = (765, 725) # Alter to the suitable size for the motor controller
     dim = (765, 635) # dimension of image (scaling factor of 2.5 to 255)
-
     img_gray_default = cv2.resize(img_gray_default, dim, interpolation = cv2.INTER_AREA)
 
-
-    img_gray = cv2.GaussianBlur(img_gray_default, (5, 5), cv2.BORDER_DEFAULT) # blur image to smooth the edges and reduce noise caused by reflection (odd value only)
+    # Pre-processing of image
+    # blur image to smooth the edges and reduce noise caused by reflection (odd value only)
+    img_gray = cv2.GaussianBlur(img_gray_default, (5, 5), cv2.BORDER_DEFAULT) 
     img_coloured = cv2.cvtColor(img_gray,cv2.COLOR_GRAY2RGB) # coloured image
     img_bin =cv2.adaptiveThreshold(img_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,7,2) # binary (black and white image)
-
-    # cv2.imshow('bin', img_bin)
-    # cv2.waitKey(0)  # display window until any keypress
-    # cv2.destroyAllWindows()
 
 
 
@@ -341,14 +270,7 @@ def route_detection_main():
 
     sorted_route_coordinates = sort_route(end_points[0], end_points[1], route_coordinates)
 
-    # print("\n \n sorted route is: \n \n" , sorted_route_coordinates)
     sorted_route_coordinates_filtered = [sorted_route_coordinates[i] for i in range(len(sorted_route_coordinates)) if i%46 == 0]
-    # print("\n \n filtered route is: \n \n" , sorted_route_coordinates_filtered)
-    # x = [x[0] for x in sorted_route_coordinates_filtered]
-    # y = [y[1] for y in sorted_route_coordinates_filtered]
-
-    # cv2.waitKey(0)  # display window until any keypress
-    # cv2.destroyAllWindows()
 
     # wrting the route coordinates into a file
     file = open("coordinates.txt","wb")
@@ -362,13 +284,6 @@ def route_detection_main():
             route_packet.append(int(sorted_route_coordinates_filtered[i][j]/3))
     route_packet.append(3)
     route_packet = bytearray(route_packet)
-
-    # setting up multiprocessing
-    # queue.put(route_packet)
     serial_read(route_packet)
-    print('I have put route packet.')
 
     return
-
-# if __name__ == "__main__":
-#     route_detection_main()
